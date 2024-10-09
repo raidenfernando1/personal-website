6a1833378a555065d6d694304c6e9e836ee66a8f
raidenfernando1 <fernandoraiden6@gmail.com>
2024-10-10 00:15:52 +0800

    added vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Analytics } from "@vercel/analytics/react";
 
 import App from "./layout";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <Analytics>
+      <App />
+    </Analytics>
   </StrictMode>
 );
 
