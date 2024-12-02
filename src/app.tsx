c38ac4f4d8ee8433ccaf2ca2d5a3362b8b7db2b2
Raiden Fernando <157260780+raidenfernando1@users.noreply.github.com>
2024-12-02 12:35:43 +0800

    made HomePage responsive

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,19 @@
 import "./app.css";
 import Navbar from "./components/Navbar";
 import Home from "./pages/home";
+import styled from "styled-components";
+
+const WebsiteLayout = styled.main`
+  margin-inline: 30px;
+`;
 
 export function App() {
   return (
     <>
       <Navbar />
-      <Home />
+      <WebsiteLayout>
+        <Home />
+      </WebsiteLayout>
     </>
   );
 }
