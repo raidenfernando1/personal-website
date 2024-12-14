a3cb3e90357371617f95e308e2c6e697c04f0356
Raiden Fernando <fernandoraiden6@gmail.com>
2024-12-14 09:44:55 +0800

    code improvements

--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -1,12 +1,10 @@
-import React, { FC } from "preact/compat";
+import { FC } from "preact/compat";
 import styled from "styled-components";
 
-interface PageLayoutTypes {
-  children: React.ReactNode;
-  paddingBlock?: string;
-}
-
-const PageLayout: FC<PageLayoutTypes> = ({ children, paddingBlock }) => {
+const PageLayout: FC<{ paddingBlock?: string }> = ({
+  children,
+  paddingBlock = `10px`, // default value
+}) => {
   const Layout = styled.section`
     padding-block: ${paddingBlock};
   `;
